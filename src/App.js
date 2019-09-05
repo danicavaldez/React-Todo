5c0a96d1a737ac425cd7910d74b107dd0273b4d7
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-09-04 18:29:03 -0700

    added todo data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+const todo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 15288170773454,
+    completed: false
+  }
+]
+
 class App extends React.Component {
   render() {
     return (
